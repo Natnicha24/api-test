ed7af3939a3e4393672ee2d02370f689281c1faf
Natnicha24 <natnicha.ant@mail.kmutt.ac.th>
2023-12-11 18:21:14 +0700

    test flutter ver3 cloudinaty

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express=require("express");
 const app=express();
+const bodyParser = require('body-parser');
+const mysql = require('mysql');
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
+const fs = require('fs');
 const PORT=4000
+
+cloudinary.config({
+    cloud_name: 'doyorigcq',
+    api_key: '451532766918258',
+    api_secret: '3IjooJAlCSOKwFIxB1SVz7vW8pE',
+});
 
 app.listen(PORT,()=>{
     console.log(`API listening on PORT${PORT}`)
