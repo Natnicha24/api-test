cc3936f5cc4a0076d76b5e8680c63a996c8c21ad
Natnicha24 <natnicha.ant@mail.kmutt.ac.th>
2023-12-11 18:37:44 +0700

    try mysql no port

--- index.js
+++ index.js
@@ -35,7 +35,6 @@ const con = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT
   });
 
 con.connect(err => {
